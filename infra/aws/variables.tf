990ffbdab1f953e720d6ea3b6fb7fdbea9d9c562
Jacob McSwain <jacob@mcswain.dev>
2023-09-13 22:07:18 -0500

    tf: remove aws profile

--- infra/aws/variables.tf
+++ infra/aws/variables.tf
@@ -3,11 +3,6 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "aws_profile" {
-  type    = string
-  default = "default"
-}
-
 variable "lambda_function_name" {
   type    = string
   default = "email-blacklist"
